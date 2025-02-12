014b3e40e5dca66fa654c0980a69fc1cf97fb502
Irish Prajapati <irishprajapati@192.168.1.4>
2025-02-12 14:24:19 +0545

    created migrations and updated models

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 # Create your models here.
 class Category(models.Model):
@@ -23,6 +24,30 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE) #user are imported
     content = models.TextField() #blank because content words can be in huge range
     created_at = models.DateTimeField(auto_now_add= True) #enables to show the created time
+    updated_at = models.DateTimeField(auto_now= True) #enables to show the updated time
+    published = models.BooleanField(default=False) #post are not published at first
+    categories = models.ManyToManyField(Category) #post can have multiple categories
+    tags = models.ManyToManyField(Tag) #post can have multiple tags 
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+    saved_by = models.ManyToManyField(User, related_name='saved_posts', blank=True) #it tracks the saved post by the user
     
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name='comments')
+    author = models.ForeignKey(User,on_delete = models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
     
