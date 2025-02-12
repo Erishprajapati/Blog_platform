5bdbe64499db6b08f725098c2bc7bb96e3668415
Irish Prajapati <irishprajapati@192.168.1.4>
2025-02-12 15:14:21 +0545

    Updated urls

--- blog_platform/settings.py
+++ blog_platform/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--knue0n#dkpng)3kqi%0)-*%jl(wk3u+(q4a3thk4ge_s6%57r'
+SECRET_KEY = os.getnenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'blog_platform.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR), ("templated")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
