aa0d7e7ec491a69fa36146ec8a1541ece4112b37
Irish Prajapati <irishprajapati@Irishs-MacBook-Air.local>
2025-02-13 06:47:37 +0545

    Navbar correction

--- blog-frontend/src/Navbar.jsx
+++ blog-frontend/src/Navbar.jsx
@@ -1,20 +1,46 @@
 import React from 'react';
-import './index.css'; // Import the custom CSS file
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary px-3">
       <div className="container-fluid">
-        {/* Left-aligned logo */}
-        <a className="navbar-brand" href="#">Blog Platform</a>
+        
+        <a className="navbar-brand fw-bold" href="#">
+         <img src="mindrisers/image/Screenshot 2025-02-13 at 06.37.47.png" alt="logo" height={40} />
 
-        {/* Right-aligned links */}
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav ms-auto"> {/* ms-auto is used to push items to the right */}
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-            <a className="nav-link" href="#">Create Post</a>
-            <a className="nav-link" href="#">Saved Posts</a>
-          </div>
+        </a>
+
+        {/* Navbar Toggler Button for Mobile */}
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav" 
+          aria-controls="navbarNav" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+    
+        <div className="collapse navbar-collapse" id="navbarNav">
+
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">Create Post</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="#">Saved Posts</a>
+            </li>
+            <li className="nav-item">
+              <button className="btn btn-light ms-3">Account</button>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
